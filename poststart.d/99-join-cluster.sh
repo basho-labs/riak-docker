7e253776d59b7a457df8d9ffaf2eedc985b956e2
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-05-31 16:19:43 +0300

    refactor: Update cluster join method
    Co-authored-by: Sergey Yelin <elinsn@gmail.com>

--- poststart.d/99-join-cluster.sh
+++ poststart.d/99-join-cluster.sh
@@ -4,5 +4,7 @@
 if [[ -z "$($RIAK_ADMIN cluster status | egrep $COORDINATOR_NODE_HOST)" && "$COORDINATOR_NODE_HOST" != "$HOST" ]]; then
   # Not already in this cluster, so join
   echo "Connecting to cluster coordinator $COORDINATOR_NODE"
-  curl -sSL $HOST:8098/admin/control/clusters/default/join/riak@$COORDINATOR_NODE_HOST
+  riak-admin cluster join $CLUSTER_NAME@$COORDINATOR_NODE
+  riak-admin cluster plan
+  riak-admin cluster commit
 fi
